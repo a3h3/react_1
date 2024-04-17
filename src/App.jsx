c208ad39031e13aba70804bf9005b020d523f606
Ahmed_Yasser <99041969+a3h3@users.noreply.github.com>
2024-04-18 00:29:15 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="body">
+      <div className="container">
+      <div className='div_1'>
+      <p>90% * 10vh</p>
+    </div>
+    <div className='div_2'>
+      <p>90% * 50vh</p>
+    </div>
+    <div className="container_2">
+    <div className='div_3'>
+      <p>300 * 300</p>
+    </div>
+    <div className='div_3'>
+      <p>300 * 300</p>
+    </div>
+    </div>
+    <div className='div_1'>
+      <p>90% * 10vh</p>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default App;
